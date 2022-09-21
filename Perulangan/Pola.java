9f1f2aea1f4308eac91bd9e6917d7b3620582d33
cstian23 <christiansuherli23@gmail.com>
2022-09-21 13:17:11 +0700

    Nambahin folder praktek

---
+++ Perulangan/Pola.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Pola
+{
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukan nilai n = ");
+        n = in.nextInt();
+        int i = 1;
+        while (i <= n)
+        {
+            int j = 1;
+            while (j <= n)
+            {
+                if ((j<=i || j>=((n+1)-i)) && (j>=i || j<=((n+1)-i)))
+                {
+                    System.out.print("*");
+                } else
+                {
+                    System.out.print(" ");
+                }
+                j += 1;
+            }
+            System.out.println("");
+            i += 1;
+        }
+    }
+}
